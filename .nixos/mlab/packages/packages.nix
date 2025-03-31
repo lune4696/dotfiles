e04b47d6edc807e285872f3d8483082532f5d8af
Lune4696 <lune.sqnm@gmail.com>
2025-03-31 09:10:45 +0900

    modify mlab config and add #mlab to flake.nix

--- .nixos/mlab/packages/packages.nix
+++ .nixos/mlab/packages/packages.nix
@@ -5,6 +5,29 @@
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "nvtopPackages.full"
+      "nvidia-x11"
+      "cuda-merged"
+      "cuda_cuobjdump"
+      "cuda_gdb"
+      "cuda_nvcc"
+      "cuda_nvdisasm"
+      "cuda_nvprune"
+      "cuda_cccl"
+      "cuda_cudart"
+      "cuda_cupti"
+      "cuda_cuxxfilt"
+      "cuda_nvml_dev"
+      "cuda_nvrtc"
+      "cuda_nvtx"
+      "cuda_profiler_api"
+      "cuda_sanitizer_api"
+      "libcublas"
+      "libcufft"
+      "libcurand"
+      "libcusolver"
+      "libnvjitlink"
+      "libcusparse"
+      "libnpp"
     ];
   environment = {
     pathsToLink = ["/libexec"];
