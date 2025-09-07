5ecb2b8c1d2d0ebc0172c26c56504bd5c349fc79
lune4696 <lune.sqnm@gmail.com>
2025-09-07 23:18:41 +0900

    update .doom.d/* to enable to use org mode more easily

--- fedora42/.doom.d/config.el
+++ fedora42/.doom.d/config.el
@@ -88,3 +88,34 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+
+;; org mode の設定
+;; ファイルの場所
+(setq org-directory "~/Documents/org/")
+;;(setq org-directory "~/Dropbox/Org")
+(setq org-default-notes-file "notes.org")
+
+;; Org-captureの設定
+
+;; Org-captureを呼び出すキーシーケンス
+(define-key global-map "\C-cc" 'org-capture)
+;; Org-captureのテンプレート（メニュー）の設定
+(setq org-capture-templates
+      '(("n" "Note" entry (file+headline "~/Documents/org/notes.org" "Notes")
+         "* %?\nEntered on %U\n %i\n %a")
+        ))
+
+;; メモをC-M-^一発で見るための設定
+;; https://qiita.com/takaxp/items/0b717ad1d0488b74429d から拝借
+(defun show-org-buffer (file)
+  "Show an org-file FILE on the current buffer."
+  (interactive)
+  (if (get-buffer file)
+      (let ((buffer (get-buffer file)))
+        (switch-to-buffer buffer)
+        (message "%s" file))
+    (find-file (concat "~/Documents/org/" file))))
+
+(global-set-key (kbd "\C-cs") '(lambda () (interactive)
+                                 (show-org-buffer "notes.org")))
